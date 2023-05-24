7f562e5f6a0317a99f4cd2acdba796de699dbdc7
leilaashur <leilahashur@gmail.com>
2023-05-24 16:30:02 +0300

    Dom commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// Change document background color to silver
+
+ 
+document.getElementById('page').style.backgroundColor='silver';
+
+// Change the font color for h1 title tag to green
+document.getElementById('title').style.color ='green'
+
+// Change the font case for h3 title tags to uppercase
+document.getElementById('fru').textTransform='uppercase'
+document.getElementById('vege').textTransform='uppercase'
+
+
+
+
+
+
+
